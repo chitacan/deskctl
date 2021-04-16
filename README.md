6c06c66f3336a90fdf029d3f9ddbfb142efa54df
Kyung Yeol Kim <kim.kyungyeol@croquis.com>
2021-04-16 09:58:46 +0900

    init.

---
+++ README.md
@@ -0,0 +1,13 @@
+# deskctl
+
+ㅠ motion desk (eg. [DSMD714N](https://www.desker.co.kr/product/DSMD714N/WWWW)) controller.
+
+control LINAK DESKLINE based motion desk with [usb connection]().
+
+## development
+
+```
+$ npm i
+$ npm link
+$ deskctl --help
+```
---
+++ bin/index.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+import { createRequire } from "module";
+import { getStatus, moveTo } from "../lib/index.js";
+
+const meow = createRequire(import.meta.url)("meow");
+
+const cli = meow(
+  `
+  Usage
+    $ deskctl <value>
+
+  Options
+    --status, -s Get current status
+    --type,   -t Type of value (cm, %, raw)
+
+  Examples
+    $ deskctl 10 -t cm
+    $ deskctl 10 -t %
+`,
+  {
+    flags: {
+      type: {
+        type: "string",
+        default: "cm",
+        alias: "t",
+      },
+      status: {
+        type: "boolean",
+        alias: "s",
+      },
+    },
+  }
+);
+
+const {
+  input: [value],
+  flags: { type, status },
+} = cli;
+
+if (type !== "cm" && type !== "%" && type !== "raw") {
+  console.error(`type ${type} is not supported`);
+  process.exit(1);
+} else if (status) {
+  getStatus()
+    .then(({ ref1 }) => {
+      if (type === "raw") {
+        console.log(
+          `${ref1.getPos()}, ${ref1.getPos("cm")}cm, ${ref1.getPos("%")}%`
+        );
+      } else {
+        console.log(ref1.getPos(type) + type);
+      }
+      process.exit(0);
+    })
+    .catch((err) => console.error(err));
+} else {
+  moveTo({ value: +value, type })
+    .then(() => process.exit(0))
+    .catch((err) => console.error(err));
+}
